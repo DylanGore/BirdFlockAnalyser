0eae59ce67a2f573f1b22f58543b993618bf4dee
Dylan Gore <hello@dylangore.ie>
2019-03-11 09:47:45 +0000

    A standard array is now used for image analysis rather than a 2D array

--- src/ie/dylangore/dsa2/ca1/ProcessImage.java
+++ src/ie/dylangore/dsa2/ca1/ProcessImage.java
@@ -11,6 +11,13 @@ import java.util.Arrays;
 
 public class ProcessImage {
 
+    /**
+     * Make image black and white
+     * @param image input image in color
+     * @param width image width
+     * @param height image height
+     * @return B&W image
+     */
     public static WritableImage makeBW(Image image, double width, double height) {
         WritableImage wImage = new WritableImage((int) width, (int) height);
         PixelReader pReader = image.getPixelReader();
@@ -19,7 +26,6 @@ public class ProcessImage {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 Color currColor = pReader.getColor(x, y);
-//                        System.out.println("RED: " + currColor.getRed() + " GREEN: " + currColor.getGreen() + " BLUE: " + currColor.getBlue());
                 if (currColor.getRed() >= 0.5 || currColor.getGreen() >= 0.5 || currColor.getBlue() >= 0.5) {
                     pWriter.setColor(x, y, new Color(1, 1, 1, 1));
                 } else {
@@ -31,29 +37,44 @@ public class ProcessImage {
         return wImage;
     }
 
+    /**
+     * Count all black and white pixels in an image
+     * @param image input image in B&W
+     * @param width image width
+     * @param height image height
+     */
     public static void processSets(Image image, int width, int height){
         DisjointSets.createSets(width, height);
-        int[][] imageSet = DisjointSets.getImageSet();
+        int[] imageSet = DisjointSets.getImageSet();
+        int whiteCount = 0;
+        int blackCount = 0;
 
-        System.out.println("Array Size: " + imageSet.length + "\n");
+        System.out.println("Array Size: " + imageSet.length);
 
         WritableImage wImage = new WritableImage((int) width, (int) height);
         PixelReader pReader = image.getPixelReader();
-        PixelWriter pWriter = wImage.getPixelWriter();
+        int currPixel = 0;
 
-        for(int y = 0; y < height; y++){
-            for(int x = 0; x < width; x++){
-                Color currColor = pReader.getColor(x, y);
-//                System.out.println("currColor: " + currColor);
-                if(currColor.equals(Color.WHITE)){
-                    imageSet[x][y] = 0;
-                }else{
-                    imageSet[x][y] = 1234;
+//        if(currPixel <= imageSet.length){
+            for(int y = 0; y < height; y++){
+                for(int x = 0; x < width; x++){
+                    Color currColor = pReader.getColor(x, y);
+                    if(currColor.equals(Color.WHITE)){
+                        imageSet[currPixel] = 0;
+                        whiteCount++;
+                    }else{
+                        imageSet[currPixel] = 1;
+                        blackCount++;
+                    }
+                    currPixel++;
                 }
             }
-        }
+//        }
 
+        // Print Results
         DisjointSets.setImageSet(imageSet);
-//        System.out.println(Arrays.deepToString(imageSet));
+        System.out.println("Image Width: " + width + " Image Height: " + height);
+        System.out.println("Black: " + blackCount);
+        System.out.println("White: " + whiteCount);
     }
 }
--- src/ie/dylangore/dsa2/ca1/sets/DisjointSets.java
+++ src/ie/dylangore/dsa2/ca1/sets/DisjointSets.java
@@ -2,25 +2,25 @@ package ie.dylangore.dsa2.ca1.sets;
 
 public class DisjointSets {
 
-    private static int[][] imageSet;
+    private static int[] imageSet;
 
     public static void createSets(int width, int height){
-        imageSet = new int[width][height];
+        imageSet = new int[width*height];
     }
 
-    public static int[][] getImageSet() {
+    public static int[] getImageSet() {
         return imageSet;
     }
 
-    public static void setImageSet(int[][] imageSet) {
+    public static void setImageSet(int[] imageSet) {
         DisjointSets.imageSet = imageSet;
     }
 
-    public static void printImageSet(){
-        for(int i = 0; i < imageSet[0].length; i++){
-            for(int j = 0; i < imageSet.length; j++){
-                System.out.println(imageSet[j][i]);
-            }
-        }
-    }
+//    public static void printImageSet(){
+//        for(int i = 0; i < imageSet[0].length; i++){
+//            for(int j = 0; i < imageSet.length; j++){
+//                System.out.println(imageSet[j][i]);
+//            }
+//        }
+//    }
 }
